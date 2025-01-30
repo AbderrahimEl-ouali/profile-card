c652d046781c8d5a503597b5715ffeb2218001c8
Abderrahim El Ouali <dev.abderrahimelouali@gmail.com>
2025-01-31 00:55:03 +0100

    V2

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 function App() {
   const skillsList = [
-    "HTML & CSS",
-    "JAVASCRIPT",
-    "REACT JS",
-    "NODE & EXPRESS JS",
-    "MONGODB & SQL",
-    "PHP LARAVEL",
-    "GIT",
-    "PYTHON",
+    { name: "HTML & CSS", level: "advanced", color: "#02e802" },
+    { name: "JAVASCRIPT", level: "advanced", color: "#e70f0f" },
+    { name: "REACT JS", level: "intermediate", color: "#0b6fe2" },
+    { name: "NODE & EXPRESS JS", level: "intermediate", color: "#ed8209" },
+    { name: "MONGODB & SQL", level: "intermediate", color: "#8504f6" },
+    { name: "PHP", level: "beginner", color: "#04eed7" },
+    { name: "GIT", level: "advanced", color: "#7f4343" },
+    { name: "PYTHON", level: "beginner", color: "#ef0bd4" },
   ];
 
   return (
     <Profile
       name="Abderrahim El Ouali"
-      image={`${process.env.PUBLIC_URL}/cropped_image.jpg`} 
+      image="cropped_image.jpg"
       title="Full-Stack Web Developer"
       description="MERN Stack | PHP & Laravel | Student at ISTA Tinghir. Passionate about building scalable, user-friendly, and efficient web solutions tailored to client needs."
       skills={skillsList}
@@ -30,15 +30,27 @@ function Profile({ name, image, title, description, skills }) {
       <p className="description">{description}</p>
       <div className="skills">
         {skills.map((skill, index) => (
-          <Skill key={index} name={skill} />
+          <Skill
+            key={index}
+            name={skill.name}
+            level={skill.level}
+            color={skill.color}
+          />
         ))}
       </div>
     </div>
   );
 }
 
-function Skill({ name }) {
-  return <div className="skill">{name}</div>;
+function Skill({ name, level, color }) {
+  const emoji =
+    level === "beginner" ? "👶" : level === "intermediate" ? "👍" : "💪";
+
+  return (
+    <div className="skill" style={{ backgroundColor: color }}>
+      {name} {emoji}
+    </div>
+  );
 }
 
 export default App;
